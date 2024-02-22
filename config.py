c50deb80081534ee4341d5b8cf9d06d7dc94db6e
OXYGEN XD <129831825+PRADHAN474@users.noreply.github.com>
2024-02-22 17:08:23 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+class Config:
+    BOT_TOKEN = "your_bot_token_here"
+    SUPPORT_GROUP_LINK = "your_support_group_link_here"
+    LOGS_GROUP_LINK = "your_logs_group_link_here"
+    OWNER_ID = "your_owner_id_here"
+    COPYRIGHT_MESSAGES = ["message1", "message2", "message3"]
