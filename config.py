d01b0289859bf334899a5c87fa6c6341b356bfa5
OXYGEN XD <129831825+PRADHAN474@users.noreply.github.com>
2024-02-22 17:24:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    BOT_TOKEN = "your_bot_token_here"
-    SUPPORT_GROUP_LINK = "your_support_group_link_here"
-    LOGS_GROUP_LINK = "your_logs_group_link_here"
-    OWNER_ID = "your_owner_id_here"
+    BOT_TOKEN = "7167596392:AAHCoW0eGmdMIrVMULIQdqDqv3JOrlpCvFE"
+    SUPPORT_GROUP_LINK = "-1002102765715"
+    LOGS_GROUP_LINK = "https://t.me/CHATTING_2024"
+    OWNER_ID = "5059737154"
     COPYRIGHT_MESSAGES = ["message1", "message2", "message3"]
